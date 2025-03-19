e14274d09153356ccf087d4805ce511841ecdd3a
Nikolay Zakharchenko <nikolay@raccoons.dev>
2025-03-19 23:03:18 +0100

    update version

--- Package.swift
+++ Package.swift
@@ -22,8 +22,8 @@ let package = Package(
         ),
         .binaryTarget(
             name: "ChainKitFFI",
-            url: "https://github.com/jup-ag/chainkit/releases/download/1.2.15/ChainKitFFI-1.2.15.zip",
-            checksum: "4da47a937a716a38d12a4bc98d801c3c1cd3bf5d00f532547abbf4ae53d8203a"
+            url: "https://github.com/jup-ag/chainkit/releases/download/1.2.17/ChainKitFFI-1.2.17.zip",
+            checksum: "943a8eb08eb361c69c2afd3f51519dd133560e8df96cb50e30216bbe212ddbdb"
         )
     ]
 )
