f213e7b9c3a67c9b933f26e8f982438722b6a4a5
codifilo <codifilo@icloud.com>
2025-07-01 12:15:57 +0200

    make YOLO version=1.2.20

--- Package.swift
+++ Package.swift
@@ -22,8 +22,8 @@ let package = Package(
         ),
         .binaryTarget(
             name: "ChainKitFFI",
-            url: "https://github.com/jup-ag/chainkit/releases/download/1.2.19/ChainKitFFI-1.2.19.zip",
-            checksum: "fd53562b900f017005a638d40b69defe025d31ff8adbb55d1f24ed95073d2308"
+            url: "https://github.com/jup-ag/chainkit/releases/download/1.2.20/ChainKitFFI-1.2.20.zip",
+            checksum: "70a6c6026baaa435190b6cd276284a5214f044dd2ac16e175514ede023dfc652"
         )
     ]
 )
